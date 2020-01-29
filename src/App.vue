ff296fe56c9f71ebc9981f03cfafa6bbbee9cb21
Luiz Fernando Rodrigues Lemos <m1015@CLUBEMONTREAL.COM.BR>
2020-01-29 14:54:02 -0300

    Tentativa de deixar todos os componentes centralizados

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <template>
-  <div>
+  <div id="center">
     <div class="row">
       <b-img id="image" src="./assets/logo.png" class="mx-auto" fluid alt="Responsive image"></b-img>
     </div>
-    <div class="row">
-      <p class="col-sm-auto mx-auto">Coisas Para Fazer</p>
-    </div>
+    <CoisasParaFazer />
     <FormDo />
   </div>
 </template>
 
 <script>
 import FormDo from "./components/form/FormDo.vue";
+import CoisasParaFazer from "./components/Layout/CoisasParaFazer.vue";
 export default {
   name: "app",
   components: {
-    FormDo
+    FormDo,
+    CoisasParaFazer
   }
 };
 </script>
 
 <style scoped>
-#image {
-  padding: 10px;
+#center {
+  height: 100%;
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/Layout/CoisasParaFazer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <b-row align-h="between">
+      <b-col cols="4">One of two columns</b-col>
+      <b-col cols="4">One of two columns</b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
