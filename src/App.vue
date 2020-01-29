2f4fd95379cfca6d6a13e7a521a9e81f852d50c1
Luiz Fernando <luizfernandorole@gmail.com>
2020-01-28 22:33:14 -0300

    Estilizando componente Form com Bootstrap-Vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-    <div class="row" >
+    <div class="row">
       <b-img id="image" src="./assets/logo.png" class="mx-auto" fluid alt="Responsive image"></b-img>
     </div>
-      <FormDo/>
+    <div class="row">
+      <p class="col-sm-auto mx-auto">Coisas Para Fazer</p>
+    </div>
+    <FormDo />
   </div>
 </template>
 
 <script>
-import FormDo from './components/form/FormDo.vue'
+import FormDo from "./components/form/FormDo.vue";
 export default {
   name: "app",
   components: {
--- src/components/form/FormDo.vue
+++ src/components/form/FormDo.vue
@@ -1,6 +1,23 @@
 <template>
-  <div class="mx-auto col-5">
-    <b-form-tags v-model="value" class="mb-2" placeholder="Tasks para fazer..."></b-form-tags>
+  <div class="mx-auto col-8 label-cols-sm-16">
+    <b-form-tags
+      v-model="value"
+      class="label-cols-sm-16 mb-2"
+      remove-on-delete
+      placeholder="Tarefas para fazer..."
+      tag-variant="info"
+      tag-pills
+      add-button-variant="info"
+    ></b-form-tags>
+    <div class="row">
+      <b-button class="col-sm-3 p-2" pill variant="outline-success">
+        <b-icon font-scale="1" class="p-10" icon="check-box" />Adicionar
+      </b-button>
+      <p class="col-sm-6"></p>
+      <b-button class="col-sm-3 p-2" pill @click="clearValue" variant="outline-danger">
+        <b-icon font-scale="1" icon="trash-fill" />Limpar
+      </b-button>
+    </div>
     <p>Value: {{ value }}</p>
   </div>
 </template>
@@ -11,6 +28,11 @@ export default {
     return {
       value: []
     };
+  },
+  methods: {
+    clearValue() {
+      this.value = [];
+    }
   }
 };
 </script>
